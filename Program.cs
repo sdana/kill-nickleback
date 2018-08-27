66e7d9d509a1f93c568a64e03dfa0a0f4a178fa8
Seth Dana <seth.dana@gmail.com>
2018-08-27 10:23:09 -0500

    exercise complete

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickleback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string name, string song)> goodSongs = new List<(string name, string song)>();
+            HashSet<(string name, string song)> allSongs = new HashSet<(string, string)>(){
+                {("The Band", "The Weight")},
+                {("Led Zeppelin", "Black Dog")},
+                {("Nickleback", "Burn It To The Ground")},
+                {("Nickleback", "Feelin' Way Too Damn Good")},
+                {("Nickleback", "Rockstar")},
+                {("Grateful Dead", "Truckin'")},
+                {("Tom Petty", "Wildflowers")},
+            };
+
+            foreach ((string name, string song) song in allSongs)
+            {
+                if (song.name != "Nickleback")
+                {
+                    goodSongs.Add((song.name, song.song));
+                }
+            }
+
+            foreach ((string name, string song) song in goodSongs)
+            {
+                System.Console.WriteLine($"{song.name} sings {song.song} and it's good!");
+            }
+            
+        }
+    }
+}
